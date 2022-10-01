ac5eed6d792f511d0fa538d4fc4d7dc9a7cae519
victor.rodriguez4.contractor <victor.rodriguez4.contractor@bbva.com>
2022-10-01 18:37:00 -0500

    feat(app.js, style.css): project and event management

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) {
   if (msg.text === 'click') {
-    async function getUsers(url) {
+
+    async function getData(url) {
       try {
         const response = await fetch(url, { method: 'GET' });
         const responseJSON = await response.json();
@@ -10,229 +11,366 @@ chrome.runtime.onMessage.addListener(async function (msg, sender, sendResponse) 
       }
     }
 
-    let users = await getUsers('http://localhost:3001/api/users/Atenea');
-
     const ghx_header = document.getElementById('ghx-header');
-
-    ghx_header.innerHTML += `<div id="daily-master" class="container">
-    <section class="timer">
-    <label id="countdown_minutes" class="timer_label">00</label>
-    <label id="countdown_seconds" class="timer_label">00</label>
-    </section>
-    
-    <section>
-    <button id="btn_countdown" class="btn btn_primary btn_start">START</button>
-    <button id="btn_create_events" class="btn btn_primary btn_generate">GENERATE</button>
-    </section>
-    
-    <section class="div_secondary">
-    <button id="btn_users" class="btn btn_red btn_users">USERS</button>
-    <input type="text" class="input_add_user" name="add user" placeholder="Add new user" id="add_user">
-    </section>
-    
-    <section class="flex div_secondary">
-    <button id="btn_add_user" class="btn btn_secondary btn_red">ADD</button>
-    
-    <button type="button" id="btn_calendar" class="btn_calendar">
-    <img id="img_calendar" height ="44" width="50" />
-    </button>
-    </section>
-    
-    
-    <div id="divListUsers" class="usersStyle hidden">
-    <ul>
-    </ul>
-    </div>
-    <div id="calendar" class="calendarStyle hidden"></div>
-    </div>`;
-    
     const ghx_modes_tools = document.getElementById('ghx-modes-tools');
     const ghx_view_selector = document.getElementById('ghx-view-selector');
-    const countdown_minutes = document.getElementById('countdown_minutes');
-    const countdown_seconds = document.getElementById('countdown_seconds');
-    const btn_countdown = document.getElementById('btn_countdown');
-    
-    const btn_calendar = document.getElementById('btn_calendar');
-    const btn_users = document.getElementById('btn_users');
-    const btn_create_events = document.getElementById('btn_create_events');
-    const divListUsers = document.getElementById('divListUsers');
-    const listUsers = divListUsers.querySelector('ul');
-    const img_calendar = document.getElementById('img_calendar');
-    const btn_add_user = document.getElementById('btn_add_user');
-    const inputAdd_user = document.getElementById('add_user');
-    let btn_delete = '';
 
-    img_calendar.src = chrome.runtime.getURL("images/calendar.png");
-    
     ghx_header.setAttribute("style","display: grid;grid-template-columns: repeat(4, 1fr);grid-auto-flow: dense;direction: rtl;");
     ghx_modes_tools.setAttribute("style","direction: ltr");
     ghx_view_selector.setAttribute("style","display: grid;grid-column: span 2;direction: ltr");
-   
-    const createListUsers = (users) => {
-      listUsers.innerHTML = '';
-      for (const user of users) {
-        listUsers.innerHTML += `<li>user ${user.name} <button class="btn_delete" id="${user._id}">Delete</button></li>`
-      }
-      btn_delete = document.querySelectorAll('.btn_delete');
 
-      for (var i=0;i<btn_delete.length;i++) {
-        btn_delete[i].onclick=function(event) {
-          event.preventDefault();
-          deleteUser(event.target.id)
+    async function showProject(){
+      ghx_header.innerHTML += `<div id="daily-master" class="container one_colum">   
+      <section id="select_projects">
+        <select name="" id="listprojects" class="select_project"></select>
+        <div class="separator">or</div>
+        <div class="add_project">
+          <input id="project" placeholder="Add new project">
+          <button id="btn_add_project" class="btn btn_red btn_width">ADD</button>
+        </div>
+      </section>
+      </div>`;
+
+      const listprojects = document.getElementById('listprojects');
+      const inputProject = document.getElementById('project');
+      const btn_add_project = document.getElementById('btn_add_project');
+      
+      const select_projects = document.getElementById('select_projects');
+
+      btn_add_project.addEventListener('click', addProject);
+
+      let projects = await getData('http://localhost:3001/api/projects');
+
+      const createListProjects = (projects) => {
+        listprojects.innerHTML = '';
+        listprojects.innerHTML = '<option value="none" selected disabled hidden>Select an Project</option>';
+        for (const project of projects) {
+          listprojects.innerHTML += `<option value="${project.name}" id="${project._id}">${project.name}</option>`
         }
-      }
-    } 
-    
-    
+      } 
 
-    function deleteUser(id){
-      fetch(`http://localhost:3001/api/users/${id}`, {
+      if(projects){
+        createListProjects(projects);
+      }
+
+      function addProject () {
+        const inputValue = inputProject.value;
+
+        if(!inputValue){
+          console.log('Escriba el nombre del usuario.')
+          return
+        }
+
+        const project = {
+          "name": inputValue,
+        };
+
+        fetch('http://localhost:3001/api/projects', {
+          method: 'POST',
+          body: JSON.stringify(project),
+          headers:{
+            'Content-Type': 'application/json'
+          }
+        }).then(res => res.json())
+        .then(async () => {
+          projects = await getData('http://localhost:3001/api/projects');
+          
+          createListProjects(projects);
+          inputProject.value = '';
+        })
+        .catch(error => console.error('Error:', error));
+
+      }
+
+      listprojects.addEventListener('change', (event)=>{
+        const daily_master = document.getElementById('daily-master');
+        const projectName = event.target.value;
+        const projectId = event.target.options[event.target.selectedIndex].id;
+
+        daily_master.classList.remove('one_colum');
+        daily_master.classList.add('two_colum');
+        select_projects.remove();
+        counter(projectName, projectId, daily_master);
+      })
+    }
+
+    showProject();
+
+    async function counter(projectName, projectId, daily_master){
+    
+      let users = await getData(`http://localhost:3001/api/users/${projectName}`);
+
+      daily_master.innerHTML += `<section id="project" class="project_selected">
+      <select><option value="none" selected="" disabled="" hidden=""></option>
+      <option value="change">Change</option>
+      <option value="delete">Delete</option>
+      </select>
+      <label>${projectName}</label>
+      </section>
+      <section class="timer">
+      <label id="countdown_minutes" class="timer_label">00</label>
+      <label id="countdown_seconds" class="timer_label">00</label>
+      </section>
+      
+      <section>
+      <button id="btn_countdown" class="btn btn_primary btn_start">START</button>
+      <button id="btn_create_events" class="btn btn_primary btn_generate">GENERATE</button>
+      </section>
+      
+      <section class="div_secondary">
+      <button id="btn_users" class="btn btn_red btn_users">USERS</button>
+      <input type="text" class="input_add_user" name="add user" placeholder="Add new user" id="add_user">
+      </section>
+      
+      <section class="flex div_secondary">
+      <button id="btn_add_user" class="btn btn_secondary btn_red">ADD</button>
+      
+      <button type="button" id="btn_calendar" class="btn_calendar">
+      <img id="img_calendar" height ="44" width="50" />
+      </button>
+      </section>
+      
+      
+      <div id="divListUsers" class="usersStyle hidden">
+      <ul>
+      </ul>
+      </div>
+      <div id="calendar" class="calendarStyle hidden"></div>`;
+
+      const selectedProject = document.getElementById('project');
+
+      selectedProject.addEventListener('change', (event) => {
+        const optionSelected = event.target.value;
+
+        if( optionSelected === 'change'){
+          daily_master.classList.remove('two_colum');
+          daily_master.classList.add('one_colum');
+          daily_master.remove();
+          showProject();
+        }
+
+        if(optionSelected === 'delete'){
+          daily_master.classList.remove('two_colum');
+          daily_master.classList.add('one_colum');
+          daily_master.remove();
+          deleteProject();
+        }
+
+        function deleteProject(){
+          fetch(`http://localhost:3001/api/projects/${projectId}`, {
+            method: 'DELETE',        
+          })
+          .then(res => res.json())
+          .then(async (data) => {
+            if(data.deletedCount){
+              showProject();
+            }
+          })
+          .catch(error => console.error('Error:', error));
+        }
+      })
+
+      const countdown_minutes = document.getElementById('countdown_minutes');
+      const countdown_seconds = document.getElementById('countdown_seconds');
+      const btn_countdown = document.getElementById('btn_countdown');
+      
+      const btn_calendar = document.getElementById('btn_calendar');
+      const btn_users = document.getElementById('btn_users');
+      const btn_create_events = document.getElementById('btn_create_events');
+      const divListUsers = document.getElementById('divListUsers');
+      const listUsers = divListUsers.querySelector('ul');
+      const img_calendar = document.getElementById('img_calendar');
+      const btn_add_user = document.getElementById('btn_add_user');
+      const inputAdd_user = document.getElementById('add_user');
+      let btn_delete = '';
+
+      img_calendar.src = chrome.runtime.getURL("images/calendar.png");
+      
+      
+    
+      const createListUsers = (users) => {
+        listUsers.innerHTML = '';
+        for (const user of users) {
+          listUsers.innerHTML += `<li>user ${user.name} <button class="btn_delete" id="${user._id}">Delete</button></li>`
+        }
+        btn_delete = document.querySelectorAll('.btn_delete');
+
+        for (var i=0;i<btn_delete.length;i++) {
+          btn_delete[i].onclick=function(event) {
+            event.preventDefault();
+            deleteUser(event.target.id)
+          }
+        }
+      } 
+      
+      
+
+      function deleteUser(id){
+        fetch(`http://localhost:3001/api/users/${id}`, {
           method: 'DELETE',        
         })
         .then(res => res.json())
         .then(async (data) => {
           if(data.deletedCount){
-            users = await getUsers('http://localhost:3001/api/users/Atenea');
+            users = await getData(`http://localhost:3001/api/users/${projectName}`);
             createListUsers(users);
           }
         })
         .catch(error => console.error('Error:', error));
       }
-    
-    if(users){
-      createListUsers(users)
-    }
-
-    const startingMinutes = 0;
-    let time = startingMinutes * 60;
-    
-    let timer;
-    let active=false;
-    
-    const start = () => {
-      if(!active){
-        timer = setInterval(updateCoutndown, 1000);
-        active=true;
-        btn_countdown.innerHTML='PAUSE'
-        return
-      }
-      clearInterval(timer);
-      active=false;
-      btn_countdown.innerHTML='START'
-    }
-
-    const addUser = () => {
-      const inputValue = inputAdd_user.value;
-
-      if(!inputValue){
-        console.log('Escriba el nombre del usuario.')
-        return
-      }
-
-      const data = {
-        "name": inputValue,
-        "project": "Atenea"
-      };
-
-      fetch('http://localhost:3001/api/users', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      }).then(res => res.json())
-      .then(async () => {
-        users = await getUsers('http://localhost:3001/api/users/Atenea');
-        createListUsers(users);
-        inputAdd_user.value = '';
-      })
-      .catch(error => console.error('Error:', error));
-
-    }
-
-    btn_countdown.addEventListener('click',start);
-    btn_add_user.addEventListener('click', addUser)
-    
-    function updateCoutndown() {
-      const minutes = Math.floor(time / 60);
-      const seconds = time % 60;
       
-      countdown_minutes.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-      countdown_seconds.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-    
-      time++;
-    }
-    
-    
-    btn_calendar.addEventListener('click',displayCalendar);
-    btn_users.addEventListener('click', displayUsers);
-    btn_create_events.addEventListener('click', () => createEvents(users));
-    
-    function displayUsers() {
-      if (divListUsers.classList.contains('hidden')) {
-        divListUsers.classList.remove('hidden');
-      } else {
-        divListUsers.classList.add('hidden');
+      if(users){
+        createListUsers(users)
       }
-    }
 
-    // extract user from DOM
-    // const ghx_avatar_img = document.getElementsByClassName('ghx-avatar-img');
-
-    // const mySet1 = new Set()
-    
-    // for (const elem of ghx_avatar_img) {
-    //   mySet1.add(elem.alt.slice(10));
-    // }
-
-    // //const myArr = Array.from(mySet1)
-    
-    // function getRandomUser(max) {
-    //   let random = Math.round(Math.random() * max)
-    //   console.log(mySet1)
-    //   //console.log(myArr)
-    //   console.log(random)
-    //   console.log([...mySet1]);
-    // }
-    // extract user from DOM
-    
-    let events = [];
-    
-    function createEvents(users) {
-      events = Events.create(users);
-    }
-    
-    const calendarEl = document.getElementById('calendar');
-    
-    
-    function createCalendar(events = []) {
-      return new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        timeZone: 'local',
-        editable: true,
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay'
-        },
-        events: events
-      });
-    }
-    
-    let calendarShow = false;
-    
-    function displayCalendar(){
-      const calendar = createCalendar(events);
-      if(!calendarShow){
-        calendarEl.classList.remove("hidden");
-        calendarShow=true
-        calendar.render();
-        return
+      const startingMinutes = 0;
+      let time = startingMinutes * 60;
+      
+      let timer;
+      let active=false;
+      
+      const start = () => {
+        if(!active){
+          timer = setInterval(updateCoutndown, 1000);
+          active=true;
+          btn_countdown.innerHTML='PAUSE'
+          return
+        }
+        clearInterval(timer);
+        active=false;
+        btn_countdown.innerHTML='START'
       }
-    
-      calendarEl.classList.add("hidden");
-      calendarShow=false;
-    }
 
+      const addUser = () => {
+        const inputValue = inputAdd_user.value;
+
+        if(!inputValue){
+          console.log('Escriba el nombre del usuario.')
+          return
+        }
+
+        const data = {
+          "name": inputValue,
+          "project": projectName
+        };
+
+        fetch('http://localhost:3001/api/users', {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers:{
+            'Content-Type': 'application/json'
+          }
+        }).then(res => res.json())
+        .then(async () => {
+          users = await getData(`http://localhost:3001/api/users/${projectName}`);
+          createListUsers(users);
+          inputAdd_user.value = '';
+        })
+        .catch(error => console.error('Error:', error));
+
+      }
+
+      btn_countdown.addEventListener('click',start);
+      btn_add_user.addEventListener('click', addUser)
+      
+      function updateCoutndown() {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        
+        countdown_minutes.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+        countdown_seconds.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+      
+        time++;
+      }
+      
+      
+      btn_calendar.addEventListener('click',displayCalendar);
+      btn_users.addEventListener('click', displayUsers);
+      btn_create_events.addEventListener('click', () => createEvents(users));
+      
+      function displayUsers() {
+        if (divListUsers.classList.contains('hidden')) {
+          divListUsers.classList.remove('hidden');
+        } else {
+          divListUsers.classList.add('hidden');
+        }
+      }
+
+      // extract user from DOM
+      // const ghx_avatar_img = document.getElementsByClassName('ghx-avatar-img');
+
+      // const mySet1 = new Set()
+      
+      // for (const elem of ghx_avatar_img) {
+      //   mySet1.add(elem.alt.slice(10));
+      // }
+
+      // //const myArr = Array.from(mySet1)
+      
+      // function getRandomUser(max) {
+      //   let random = Math.round(Math.random() * max)
+      //   console.log(mySet1)
+      //   //console.log(myArr)
+      //   console.log(random)
+      //   console.log([...mySet1]);
+      // }
+      // extract user from DOM
+
+      let events = await getData(`http://localhost:3001/api/events/${projectName}`);
+      const eventsId = events[0]._id;     
+      
+      function createEvents(users) {
+        events = Events.create(users);
+        
+        fetch(`http://localhost:3001/api/events/${eventsId}`, {
+          method: 'PUT',
+          body: JSON.stringify(events),
+          headers:{
+            'Content-Type': 'application/json'
+          }
+        }).then(res => res.json())
+        .then(async (data) => {
+          if(data.modifiedCount){
+            events = await getData(`http://localhost:3001/api/events/${projectName}`);
+          }
+        })
+        .catch(error => console.error('Error:', error));
+      }
+      
+      const calendarEl = document.getElementById('calendar');
+      
+      
+      function createCalendar(events = []) {
+        console.log(events)
+
+        return new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+          timeZone: 'local',
+          editable: true,
+          headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+          },
+          events: events
+        });
+      }
+      
+      let calendarShow = false;
+      
+      function displayCalendar(){
+        const calendar = createCalendar(events[0].events);
+        if(!calendarShow){
+          calendarEl.classList.remove("hidden");
+          calendarShow=true
+          calendar.render();
+          return
+        }
+      
+        calendarEl.classList.add("hidden");
+        calendarShow=false;
+      }
+
+    }
   }
 });
